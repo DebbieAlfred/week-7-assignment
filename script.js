831e59960a17cc21e2011a12f47c5b908a301c83
Etini Alphonsus Alfred <etinialfred@gmail.com>
2023-09-10 17:32:26 +0100

    Class and properties

---
+++ script.js
@@ -0,0 +1,14 @@
+//create a class called Animal with properties for species and sound
+class Animal {
+    constructor(species, sound){
+       this.species = species;
+        this.sound = sound;    
+    }
+}
+//Include method
+//Create a subclass Dog 
+//Add additional property for color
+//Overide sound method to includd dog color
+//Creat instance of DOg class to make it sound 
+
+
